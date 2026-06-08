1b3ec980be03fde73d332fae1b5d7e708c9f4270
rafagmoraes-dot <rafagmoraes@outlook.com>
2026-06-08 10:53:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-
 import streamlit as st
+import geopandas as gpd
 
-st.title("Parques e Bosques de Curitiba")
+st.set_page_config(
+    page_title="Parques e Bosques de Curitiba",
+    layout="wide"
+)
 
-st.write("Meu primeiro dashboard com Streamlit")
+gdf = gpd.read_file("parques_bosques_curitiba.geojson")
+
+gdf["AREA_HA"] = gdf.geometry.area / 10000
+
+st.title("🌳 Parques e Bosques de Curitiba")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(
+        "Total de áreas verdes",
+        len(gdf)
+    )
+
+with col2:
+    st.metric(
+        "Área total (ha)",
+        round(gdf["AREA_HA"].sum(), 2)
+    )
+
+st.subheader("Tipos de áreas verdes")
+
+st.write(gdf["TIPO"].value_counts())
